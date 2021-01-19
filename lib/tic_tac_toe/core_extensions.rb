2077b48f59f8fff070078b18295fc5fe3b606492
Adam Jonas <adamjonas25@gmail.com>
2021-01-19 11:38:14 +0000

    define 2 methods in array #any_empty? and #none_empty?

--- lib/tic_tac_toe/core_extensions.rb
+++ lib/tic_tac_toe/core_extensions.rb
@@ -6,4 +6,12 @@ class Array
   def all_same?
     self.all?(self[0])
   end
+
+  def any_empty?
+    self.any { |element| element.to_s.empty? }
+  end
+
+  def none_empty?
+    !self.any_empty?
+  end
 end
