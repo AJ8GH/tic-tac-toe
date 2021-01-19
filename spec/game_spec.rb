ff5fa69f63b6aa49d3f9938ef6ba6d028481f769
Adam Jonas <adamjonas25@gmail.com>
2021-01-19 16:26:20 +0000

    define game over message method and pass specs for new method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,6 +6,8 @@ module TicTacToe
     let(:isaac) { Player.new(name: 'Isaac', character: 'O') }
     let(:expected) { "Adam: Enter a number between 1 and 9 to make your move" }
 
+    let(:draw_message) { "The game ended in a tie" }
+
     context '#initialize' do
       it 'randomly selects a current_player' do
         allow_any_instance_of(Array).to receive(:shuffle).and_return([adam, isaac])
@@ -48,6 +50,20 @@ module TicTacToe
       it 'converts human_move of 1 to [0, 0]' do
         game = Game.new([adam, isaac])
         expect(game.get_move('1')).to eq [0, 0]
+      end
+
+      it 'converts human_move of 6 to [2, 1]' do
+        game = Game.new([adam, isaac])
+        expect(game.get_move('6')).to eq [2, 1]
+      end
+    end
+
+    context '#game_over_message' do
+      it 'returns win message when player wins' do
+        game = Game.new([adam, isaac])
+        allow(game.board).to receive(:game_over) { :winner }
+        allow(game).to receive(:current_player) { adam }
+        expect(game.game_over_message).to eq "Adam won!"
       end
     end
   end
